3529805291027272e75a4bd95c92513a10f0389f
VirtualBrightPlayz <lego.trains.b@gmail.com>
2024-10-06 02:03:28 -0700

    optimize chunk data access

--- Scripts/VoxelWorldRenderer.cs
+++ Scripts/VoxelWorldRenderer.cs
@@ -10,9 +10,10 @@ public class VoxelWorldRenderer : MonoBehaviour
     public Dictionary<Vector3Int, MeshCollider> colliders = new Dictionary<Vector3Int, MeshCollider>();
     public int renderDistance = 8;
     private Camera cam;
-    private Vector3Int[] lastFrame = new Vector3Int[0];
+    private Vector3Int lastPosition = Vector3Int.zero;
     private List<Vector3Int> foundChunks = new List<Vector3Int>();
-    private List<Vector3Int> removedChunks = new List<Vector3Int>();
+    private List<Chunk> activeChunks = new List<Chunk>();
+    private int activeChunkCount;
 
     private void LateUpdate()
     {
@@ -22,55 +23,53 @@ public class VoxelWorldRenderer : MonoBehaviour
             return;
         }
         int max = (renderDistance * 2 + 1) * (renderDistance * 2 + 1) * (renderDistance * 2 + 1);
-        // if (lastFrame.Length != max)
-        //     lastFrame = new Vector3Int[max];
         foundChunks.Clear();
         foundChunks.Capacity = max;
         Vector3Int camChunkPos = VoxelWorld.FloorPosition(cam.transform.position);
-        for (int x = -renderDistance; x <= renderDistance; x++)
-            for (int y = -renderDistance; y <= renderDistance; y++)
-                for (int z = -renderDistance; z <= renderDistance; z++)
-                {
-                    if (world.TryGetChunk(camChunkPos + new Vector3Int(x, y, z), out Chunk chunk))
-                    {
-                        foundChunks.Add(chunk.chunkPosition);
-                        if (meshes.TryGetValue(chunk.chunkPosition, out VoxelMesh mesh))
-                        {
-                            if (chunk.shouldUpdate > 0)
-                            {
-                                _ = mesh.UpdateMeshAsync();
-                            }
-                            mesh.Draw();
-                            if (colliders.TryGetValue(chunk.chunkPosition, out MeshCollider collider))
-                            {
-                                collider.sharedMesh = mesh.GetMesh();
-                            }
-                            else
-                            {
-                                MeshCollider vCollider = Instantiate(colliderPrefab, VoxelWorld.UnroundPosition(chunk.chunkPosition), Quaternion.identity, transform);
-                                vCollider.sharedMesh = mesh.GetMesh();
-                                colliders.Add(chunk.chunkPosition, vCollider);
-                            }
-                        }
-                        else
-                        {
-                            VoxelMesh vMesh = new VoxelMesh(world, chunk);
-                            meshes.Add(chunk.chunkPosition, vMesh);
-                        }
-                    }
-                }
-        removedChunks.Clear();
-        foreach (var kvp in colliders)
+        if (camChunkPos != lastPosition)
         {
-            if (!foundChunks.Contains(kvp.Key))
+            foreach (var kvp in colliders)
             {
                 Destroy(kvp.Value.gameObject);
-                removedChunks.Add(kvp.Key);
+            }
+            colliders.Clear();
+            activeChunks.Clear();
+            activeChunks.Capacity = max;
+            for (int x = -renderDistance; x <= renderDistance; x++)
+                for (int y = -renderDistance; y <= renderDistance; y++)
+                    for (int z = -renderDistance; z <= renderDistance; z++)
+                        if (world.TryGetChunk(camChunkPos + new Vector3Int(x, y, z), out Chunk chunk))
+                            activeChunks.Add(chunk);
+            activeChunkCount = activeChunks.Count;
+        }
+        for (int i = 0; i < activeChunkCount; i++)
+        {
+            Chunk chunk = activeChunks[i];
+            foundChunks.Add(chunk.chunkPosition);
+            if (meshes.TryGetValue(chunk.chunkPosition, out VoxelMesh mesh))
+            {
+                if (chunk.shouldUpdate > 0)
+                {
+                    _ = mesh.UpdateMeshAsync();
+                }
+                mesh.Draw();
+                if (colliders.TryGetValue(chunk.chunkPosition, out MeshCollider collider))
+                {
+                    collider.sharedMesh = mesh.GetMesh();
+                }
+                else
+                {
+                    MeshCollider vCollider = Instantiate(colliderPrefab, VoxelWorld.UnroundPosition(chunk.chunkPosition), Quaternion.identity, transform);
+                    vCollider.sharedMesh = mesh.GetMesh();
+                    colliders.Add(chunk.chunkPosition, vCollider);
+                }
+            }
+            else
+            {
+                VoxelMesh vMesh = new VoxelMesh(world, chunk);
+                meshes.Add(chunk.chunkPosition, vMesh);
             }
         }
-        for (int i = 0; i < removedChunks.Count; i++)
-        {
-            colliders.Remove(removedChunks[i]);
-        }
+        lastPosition = camChunkPos;
     }
 }
